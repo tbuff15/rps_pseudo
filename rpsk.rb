2bb34a74d7787d4a8a716dc93bbfab685dc106a8
Theo Bufferd <tbufferd@lsoc.org>
2016-01-28 10:53:54 -0600

    mostly complete

--- rpsk.rb
+++ rpsk.rb
@@ -1,75 +1,37 @@
 best_of = 3
-player_1 wins = 0
-computer wins = 0
-
-if computer_wins < 3 and player_wins < 3
+player_wins = 0
+computer_wins = 0
+while computer_wins < 2 and player_wins < 2
 	puts "Select Rock, Paper, or Scissors"
-persons_choice = gets.chomp
-computers_choice = [rock,paper,scissors].shuffle 
-
+	persons_choice = gets.chomp
+	computers_choice = ["Rock","Paper","Scissors"].shuffle.first 
 if persons_choice == computers_choice 
-	then puts "draw"
-elsif computers_choice == "rock" and persons_choice == "paper"
-	then puts "You win"
+	then puts "Draw."
+elsif computers_choice == "Rock" and persons_choice == "Paper"
+	then puts "You win."
 	player_wins = player_wins +1
-elsif computers_choice == "rock" and persons_choice == "scissors"
-	then puts "computer wins!"
+elsif computers_choice == "Rock" and persons_choice == "Scissors"
+	then puts "Computer wins!"
 	computers_wins = computers_wins +1
-elsif computers_choice == "scissors" and persons_choice =="rock"
-	then puts "You win"
-elsif computers_choice == "scissors" and persons_choice == "paper"
+elsif computers_choice == "Scissors" and persons_choice =="Rock"
+	then puts "You win."
+	player_wins = player_wins +1
+elsif computers_choice == "Scissors" and persons_choice == "Paper"
 	then puts "computer wins!"
-elsif computers_choice == "paper" and persons_choice == "rock"
-	then puts "computer wins!"  
-elsif computers_choice == "paper" and persons_choice == "scissors"
+	computer_wins = computer_wins +1
+elsif computers_choice == "Paper" and persons_choice == "Rock"
+	then puts "computer wins!"
+	computer_wins = computer_wins +1  
+elsif computers_choice == "Paper" and persons_choice == "Scissors"
 	then puts "You win!"
-					
-			
-			
-#Declare a variable called best_of, set it equal to 3
-#Create a counter for player's wins, set it equal to 0
-#Create a counter for computer's wins, set it equal to 0
-
-
-#While computer_score and person score are less than 3
-
-	#puts Select Rock, Paper or Scissors
-	#get person's choice, set to variable persons_choice
-
-	#get computers choice randomly from an array, set to variable computers_choice
-
-	#Start an if statement: if both choices are the same
-		#Puts draw
-	#else if computer has rock and human has paper
-		#Puts You Win
-		#add a point for human
-	#else if computer has rock and human has scissors
-		#Puts Computer Wins
-		#add a point for computer
-	#else if computer has scissors and human has rock
-		#Puts You Win
-		#add a point for human
-	#else if computer has scissors and human has paper
-		#Puts Computer Wins
-		#add a point for computer
-	#else if computer has paper and human has rock
-		#Puts Computer Wins
-		#add a point for computer
-	#else if computer has paper and human has scissors
-		#Puts You Win
-		#add a point for human
-	#else
-		#puts enter a valid option and give options
-
-
-
-#If computer score == 2
-	#Puts Computer is the best of 3!
-#Else
-	#Puts Human is best of 3!
-	
-
-
-##BONUS##
-	#If time permits
-		#Create a way to show history of choices when declaring winner of 3
+	player_wins = player_wins +1
+elsif 		
+	puts "Enter a valid option: Rock, Paper, or Scissors"
+end
+if computer_wins == 2
+	then puts "Computer wins the best of 3! Final score: #{computer_wins}:#{player_wins}"
+end
+if player_wins == 2
+	then puts "Human wins the best of 3! Final score: #{computer_wins}:#{player_wins}"
+end
+end		
